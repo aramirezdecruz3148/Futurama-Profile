be9a92c899a0f01252efa3d792edacadfb7c2551
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-12 17:13:44 -0700

    was able to create the GET method to return all profiles with a passing test

--- tests/futurama.test.js
+++ tests/futurama.test.js
@@ -14,4 +14,16 @@ describe('futurama routes', () => {
         });
       });
   });
+
+  it('GET can return all profiles', () => {
+    return request(app)
+      .get('/api/v1/profiles')
+      .then(res => {
+        expect(res.body).toEqual([{ 
+          name: 'Alex',
+          favoriteCharacter: 'Bender',
+          tagline: expect.any(String) 
+        }]);
+      });
+  });
 });
