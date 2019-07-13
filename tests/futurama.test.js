8295336c25b797d18635ed7154924b0bf10bdbf9
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-12 17:01:02 -0700

    I was able to get the random quote working with a middleware function and a passing test

--- tests/futurama.test.js
+++ tests/futurama.test.js
@@ -2,16 +2,16 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('futurama routes', () => {
-  it('POST can return a profile', () => {
+  it('POST can return a profile with name and favoriteCharacter', () => {
     return request(app)
       .post('/api/v1/profiles')
       .send({ name: 'Alex', favoriteCharacter: 'Bender' })
       .then(res => {
         expect(res.body).toEqual({ 
           name: 'Alex',
-          favoriteCharacter: 'Bender'
+          favoriteCharacter: 'Bender',
+          tagline: expect.any(String)
         });
       });
   });
 });
-
